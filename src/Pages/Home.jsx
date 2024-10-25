8060a05c15f38b48c77072911d9e1495904253ee
Riyal Jain <69381468+riyaljain5@users.noreply.github.com>
2024-10-25 21:21:08 +0530

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
           animate={{ opacity: 1, y: 0 }} // Fade in and move up into place
           transition={{ duration: 0.8, ease: "easeOut" }} // Smooth and fast animation
         >
-          <h1 className="font-bold text-4xl md:text-7xl text-white text-center text-gray-800 text-w w-full md:mb-0 mb-3">
+          <h1 className="font-bold text-4xl md:text-7xl  text-center text-black text-w w-full md:mb-0 mb-3">
             Welcome to CGKeech – Your Trusted Platform for Authentic <span className='text-[#A91B60]'>Bangalore</span> Call Girl Services
           </h1>
           <div className="relative z-10 mt-5">
